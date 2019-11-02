35be30fe4d462e15b0c87d41695c53001ee33af3
Tatiana <tatianakobayashi@hotmail.com>
2019-11-02 15:54:34 -0400

    header test

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Css Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- JavaScript Bootstrap & JQuery -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>-->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
+    <link rel="stylesheet" href="css/basic.css">
