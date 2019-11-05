9af66a6b6436a56bf07c42c6bca4d4b9762e3beb
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-05 11:49:07 -0400

    Update Issue.php
    - add id
    - add edit counter

--- Models/Issue.php
+++ Models/Issue.php
@@ -8,12 +8,15 @@ class Issue{
 	private $tosUrl;
 	private $topic;
 	private $quote;
+	private $id;
+	private $edits;
 
-	function __construct($siteName, $tosUrl, $topic, $quote){
+	function __construct($siteName, $tosUrl, $topic, $quote, $edits){
 		$this->siteName = $siteName;
 		$this->tosUrl = $tosUrl;
 		$this->topic = $topic;
 		$this->quote = $quote;
+		$this->edits = $edits;
 	}
 
 	public function getSiteName()
@@ -50,5 +53,23 @@ class Issue{
 	public function setQuote($quote)
 	{
 		$this->quote = $quote;
+	}	
+
+	public function getId()
+	{
+		return $this->id;
+	}
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	public function getEdits()
+	{
+		return $this->edits;
+	}
+	public function setEdits($edits)
+	{
+		$this->edits = $edits;
 	}
 }
