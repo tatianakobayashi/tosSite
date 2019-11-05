91e6d843406c6495850af4692f31be4aff1c5419
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-05 11:53:31 -0400

    Update newTopic.php
    - insert to db
    - success message

--- newTopic.php
+++ newTopic.php
@@ -5,18 +5,28 @@
 <?php 
 require_once("navbar.php"); 
 require_once("Models/Issue.php");
+require_once("issue-database.php"); 
+  
+$issue = new Issue($_POST["siteName"], $_POST["termUrl"], $_POST["topic"], $_POST["quote"], 0);
 
-$issue = new Issue($_POST["siteName"], $_POST["termUrl"], $_POST["topic"], $_POST["quote"]);
+$success = insertIssue($connection, $issue);
 
-// Inserir no banco
-
-?> 
-
+if($success) {
+?>
+<p class="alert alert-success">Tópico adicionado com sucesso!</p>
+<?php
+} else {
+    $msg = mysqli_error($connection);
+?>
+<p class="alert alert-danger">O tópico não foi adicionado: <?= $msg ?></p>
+<?php
+}
+?>
     <div>
-        <strong>Site: </strong> <span> <?php echo $issue->getSiteName();?></span>
-        <strong>Url: </strong> <span> <?php echo $issue->getTosUrl();?></span>
-        <strong>Tópico: </strong> <span> <?php echo $issue->getTopic();?></span>
-        <strong>Citação: </strong> <span> <?php echo $issue->getQuote();?></span>
+        <strong>Site: </strong> <span> <?php echo $issue->getSiteName();?></span> <br>
+        <strong>Url: </strong> <span> <?php echo $issue->getTosUrl();?></span> <br>
+        <strong>Tópico: </strong> <span> <?php echo $issue->getTopic();?></span> <br>
+        <strong>Citação: </strong> <span> <?php echo $issue->getQuote();?></span> <br>
     </div>
 </body>
 </html>
