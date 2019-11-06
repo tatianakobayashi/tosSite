e28cb5a5c9624459d3d4c23ac179e485e3c414fd
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-06 09:02:56 -0400

    Create post-form.php

---
+++ post-form.php
@@ -0,0 +1,8 @@
+    <div>
+        <strong>Site: </strong> <span> <?php echo $issue->getSiteName();?></span> <br>
+        <strong>Url: </strong> <span> <?php echo $issue->getTosUrl();?></span> <br>
+        <strong>Tópico: </strong> <span> <?php echo $issue->getTopic();?></span> <br>
+        <strong>Citação: </strong> <span> <?php echo $issue->getQuote();?></span> <br>
+    </div>
+</body>
+</html>
