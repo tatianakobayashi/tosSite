1df7e3256154281ac77d09afa749c1665d75c91d
Tatiana <tatianakobayashi@hotmail.com>
2019-11-05 23:30:09 -0400

    action

--- topicTable.php
+++ topicTable.php
@@ -28,6 +28,7 @@ require_once("issue-database.php"); ?>
             <td>
                 <form action="edit-issue.php" method="post">
                     <input type="hidden" name="id" value="<?=$issue->getId()?>" />
+                    <input type="hidden" name="action" value="edit" />
                     <button class="btn btn-danger">Editar</button>
                 </form>
             </td>
