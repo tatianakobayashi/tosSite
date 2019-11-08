ed1d14056a03ed12c422ef287454ec9c1e300fed
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-08 07:54:36 -0400

    Update post-translation-form.php
    - code

---
+++ post-translation-form.php
@@ -0,0 +1,6 @@
+<div>
+        <p><strong>Original: </strong> <span> <?php echo $topic->getEnglish();?></span></p>
+        <p><strong>Tradução: </strong> <span> <?php echo $topic->getTranslation();?></span></p>
+    </div>
+</body>
+</html>
