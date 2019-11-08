962afb8de5c39160c5cabcde301605bcb5a7dc10
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-08 07:53:56 -0400

    Update newTranslation.php
    - code

---
+++ newTranslation.php
@@ -0,0 +1,25 @@
+<?php require_once("header.php"); ?>
+  <title>Nova Tradução</title>
+</head>
+<body>
+<?php 
+require_once("navbar.php"); 
+require_once("Models/Topic.php");
+require_once("connect.php");
+require_once("topic-database.php"); 
+  
+$topic = new Topic($_POST["english"], $_POST["translation"]);
+$success = insertTopic($connection, $topic);
+
+if($success) {
+?>
+<p class="alert alert-success">Tradução adicionada com sucesso!</p>
+<?php
+} else {
+    $msg = mysqli_error($connection);
+?>
+<p class="alert alert-danger">A tradução não foi adicionada: <?= $msg ?></p>
+<?php
+}
+?>
+<?php require_once("post-translation-form.php"); ?>
