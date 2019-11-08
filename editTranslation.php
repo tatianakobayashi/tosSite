ed8c6b9f5e1f09b5e7349da9571308a86e0b4e63
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-08 07:54:20 -0400

    Update editTranslation.php
    - code

---
+++ editTranslation.php
@@ -0,0 +1,25 @@
+<?php require_once("header.php"); ?>
+  <title>Tradução Atualizada</title>
+</head>
+<body>
+<?php 
+require_once("navbar.php"); 
+require_once("Models/Topic.php");
+require_once("connect.php");
+require_once("topic-database.php"); 
+
+$topic = new Topic($_POST["english"], $_POST["translation"]);
+$success = alterTopic($topic);
+
+if($success) {
+?>
+<p class="alert alert-success">Tradução atualizada com sucesso!</p>
+<?php
+} else {
+    $msg = mysqli_error($connection);
+?>
+<p class="alert alert-danger">A tradução não foi atualizada: <?= $msg ?></p>
+<?php
+}
+?>
+<?php require_once("post-translation-form.php"); ?>
