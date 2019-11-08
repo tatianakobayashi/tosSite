9fed9913c7da36cc19525ffc653fbc615fc77782
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-08 07:56:47 -0400

    Update translation-form.php
    - input -> textarea
    - englishTopic -> english

--- translation-form.php
+++ translation-form.php
@@ -1,31 +1,31 @@
 <?php require_once("header.php"); ?>
-	<title>Formulário</title>
+    <title>Formulário</title>
 </head>
 <body>
-	<?php
-	require_once("topic-database.php");
-	
+    <?php
+    require_once("topic-database.php");
+    
     $action = 'new';
-	if( isset($_POST['action']) ){
+    if( isset($_POST['action']) ){
      $action = $_POST['action'];
-	}
+    }
 
-	$topic = NULL;
-	if($action == 'edit'){
+    $topic = NULL;
+    if($action == 'edit'){
         $id = $_POST["id"];
         $topic = getTopicById($connection, $id);
-	}
+    }
 
-	require_once("navbar.php"); ?> 
+    require_once("navbar.php"); ?> 
     <form method="POST" action="<?php if($action == 'edit'){ echo 'editTranslation.php';} else{echo 'newTranslation.php';} ?>">
         <div class="form-group">
-            <label for="englishTopic">Tópico em inglês</label>
-            <input type="text" name="englishTopic" class="form-control" value="<?php echo (isset($topic))?$topic->getEnglish():'';?>" />
+            <label for="english">Tópico em inglês</label>
+            <textarea type="text" name="english" class="form-control"><?php echo (isset($topic))?$topic->getEnglish():'';?></textarea>
         </div>
 
         <div class="form-group">
             <label for="translation">Tradução</label>
-            <input type="text" name="translation" class="form-control" value="<?php echo (isset($topic))?$topic->getTranslation():'';?>"/>
+            <textarea type="text" name="translation" class="form-control"><?php echo (isset($topic))?$topic->getTranslation():'';?></textarea>
         </div>
         <input type="hidden" name="id" value="<?php echo (isset($id))?$id:''; ?>">
         <button type="submit" class="btn btn-primary">Enviar</button>
