8a958cf4f5c2fc613ce154ce2827f0a84e8f4c33
Tatiana <tatianakobayashi@hotmail.com>
2019-11-08 17:26:25 -0400

    con

--- editTopic.php
+++ editTopic.php
@@ -9,7 +9,7 @@ require_once("connect.php");
 require_once("issue-database.php"); 
 
 $issue = new Issue($_POST["siteName"], $_POST["termUrl"], $_POST["topic"], $_POST["quote"], $_POST["edits"]);
-$success = alterIssue($issue);
+$success = alterIssue($connection, $issue);
 if($success) {
 ?>
 <p class="alert alert-success">Tópico atualizado com sucesso!</p>
