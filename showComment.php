78fb6d943ca851818fe22363d886631e7ab4dfee
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 11:34:16 -0400

    Update showComment.php

--- showComment.php
+++ showComment.php
@@ -1,21 +1,36 @@
 <?php require_once("header.php"); ?>
-	<title>Comentário</title>
+  <title>Comentário</title>
 </head>
 <body>
     <?php 
-	require_once("Models/Comment.php"); 
-	require_once("navbar.php");
-	require_once("Database/connect.php"); 
-	require_once("Database/comment-database.php"); 
-	require_once("Database/user-database.php"); 
-	require_once("Database/issue-database.php"); 
-	
-	if(isset($_POST["commentId"])){
-		$comment = getCommentById($connection, $_POST["commentId"]);
-		$userName = getUserNameById($connection, $comment->getUserId());
-		$topic = getIssueById($connection,  $comment->getTopicId());
-	}
-	?>
+  require_once("Models/Comment.php"); 
+  require_once("navbar.php");
+  require_once("Database/connect.php"); 
+  require_once("Database/comment-database.php"); 
+  require_once("Database/user-database.php"); 
+  require_once("Database/issue-database.php"); 
+  
+  if(isset($_POST["commentId"])){
+    $comment = getCommentById($connection, $_POST["commentId"]);
+    $userName = getUserNameById($connection, $comment->getUserId());
+    $topic = getIssueById($connection,  $comment->getTopicId());
+  }
+  else if(isset($_POST) && isset($_SESSION["userId"])){
+    $sucess = insertComment($_POST["connection"], $_POST["title"], $_POST["text"], $_POST["importance"], $_POST["classification"], $_POST["topicId"], $_SESSION["userId"]);
+    if($success) {
+      $comment = new Comment($_POST["connection"], $_POST["title"], $_POST["text"], $_POST["importance"], $_POST["classification"], $_POST["topicId"], $_SESSION["userId"]);
+      $comment->setId($connection->insert_id);
+  ?>
+      <p class="alert alert-success">Comentário inserido com sucesso!</p>
+  <?php
+    } else {
+      $msg = mysqli_error($connection);
+  ?>
+      <p class="alert alert-danger">O tópico não foi inserido: <?= $msg ?></p>
+  <?php
+    }
+  }
+  ?>
     <div>
         <p><span><strong>Título:</strong><?=isset($comment)?$comment->getTitle():'Indisponível'?></span></p>
         <p><span><strong>Conteúdo:</strong><?=isset($comment)?$comment->getText():'Indisponível'?></span></p>
