9bfb97219c5a1f363f2e0553385f62836fc9cf47
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 08:25:47 -0400

    Update logout.php
    - code

--- logout.php
+++ logout.php
@@ -1,3 +1,23 @@
-<?php
 
-// Retirar usuário da sessão
+<?php
+session_unset();
+session_destroy();
+include_once('header.php');
+?>
+<title>Logout</title>
+</head>
+<body>
+  <?php include_once('navbar.php');?>
+  <div>
+    <?php if(!isset($_SESSION['userId'])){?>
+      <div class="alert alert-primary" role="alert">
+        Logout realizado com sucesso.
+      </div>
+    <?php }else{?>
+      <div class="alert alert-warning" role="alert">
+        A sessão não pôde ser terminada.
+      </div>
+    <?php } // end if/else ?>
+  </div>
+</body>
+</html>
