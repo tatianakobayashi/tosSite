697c14683e76dc53e4f36f9bdb64327455f59c01
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 10:57:17 -0400

    Create dictionaries.php

---
+++ dictionaries.php
@@ -0,0 +1,34 @@
+<?php
+$rating_to_string = array(
+  0=> "Sem classificação",
+  "false"=> "Sem classificação",
+  "A"=> "Os Termos de serviço te tratam de forma justa, respeitam seus direitos e seguem as melhores práticas.",
+  "B"=> "Os Termos de serviço são justos com o usuário, mas podem ser melhorados.",
+  "C"=> "Os Termos de serviço são bons, mas alguns problemas precisam da sua consideração.",
+  "D"=> "Os Termos de serviço são muito irregulares ou existem problemas importantes que necessitam da sua atenção.",
+  "E"=> "Os Termos de serviço levantam sérias preocupações."
+);
+
+$point_translation = array(
+  "bad"=> "Ruim",
+  "good"=> "Bom",
+  "neutral"=> "Neutro"
+);
+
+$point_alert = array(
+  "bad"=> "border-danger text-danger",
+  "good"=> "border-success text-success",
+  "neutral"=> "border-secondary text-secondary",
+  "undefined"=> "border-dark text-dark"
+);
+
+$rating_color_class = array(
+  0=> "black",
+  "false"=> "black",
+  "A"=> "green",
+  "B"=> "yellow",
+  "C"=> "orange",
+  "D"=> "red",
+  "E"=> "burgundy"
+);
+?>
