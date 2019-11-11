3be5ea5e08476bfe260b6dd4d3dd69cb057878f3
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 12:12:05 -0400

    Create showIssue.php

---
+++ showIssue.php
@@ -0,0 +1,34 @@
+<?php require_once("header.php"); 
+
+require_once("Models/Issue.php");
+require_once("Database/connect.php");
+require_once("Database/issue-database.php"); 
+
+$issue = getIssueById($connection, $_POST["issueId"]);
+?>
+  <title><?=$issue->getTopic()?></title>
+</head>
+<body>
+<?php require_once("navbar.php"); ?>
+  <h2> <?php echo $issue->getTopic();?></h2> <br>
+  <div>
+    <strong>Site: </strong> <span> <?php echo $issue->getSiteName();?></span> <br>
+    <strong>Url: </strong> <span> <?php echo $issue->getTosUrl();?></span> <br>
+    <strong>Citação: </strong> <span> <?php echo $issue->getQuote();?></span> <br>
+
+    <form action="form.php" method="post">
+      <input type="hidden" name="id" value="<?=$issue->getId()?>" />
+      <input type="hidden" name="action" value="edit" />
+      <button class="btn btn-danger" action="submit">Editar</button>
+    </form>
+
+    <form action="newComment.php" method="post">
+      <input type="hidden" name="id" value="<?=$issue->getId()?>" />
+      <input type="hidden" name="action" value="edit" />
+      <button class="btn btn-danger" action="submit">Comentar</button>
+    </form>
+  </div>
+
+  <?php // Show comments ?>
+</body>
+</html>
