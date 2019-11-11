8a9a7b986ef32f461f5faa281f80ef2eac39fc20
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 09:26:07 -0400

    Create comment-database.php

---
+++ Database/comment-database.php
@@ -0,0 +1,60 @@
+<?php
+require_once("Models/Comment.php"); // ($title, $text, $importance, $classification, $topicId, $userId)
+function insertComment($connection, $title, $text, $importance, $classification, $topicId, $userId) {
+    $query = "insert into comments (title, text, importance, classification, topicId, userId) values ('{$title}', '{$text}', {$importance}, '{$classification}', '{$topicId}', {$userId})";
+    return mysqli_query($connection, $query);
+}
+
+function removeComment($connection, $id) {
+    $query = "delete from comments where id = {$id}";
+    return mysqli_query($connection, $query);
+}
+
+function getAllComments($connection) {
+    $comments = array();
+    $result = mysqli_query($connection, "select * from comments");
+    while($arr = mysqli_fetch_assoc($result)) {
+        $comment = new Comment($arr["title"], $arr["text"], $arr["importance"], $arr["classification"], $arr["topicId"], $arr["userId"]);
+        $comment->setId($arr["id"]);
+        array_push($comments, $comment);
+    }
+    return $comments;
+}
+
+function alterComment($connection, $comment) {
+    $query = "update comments set title = '{$comment->getTitle()}', text = '{$comment->getText()}', importance={$comment->getImportance()}, classification = '{$comment->getClassification()}' where id = {$comment->getId()} and topicId = {$comment->getTopicId()} and userId = {$comment->getUserId()}";
+    return mysqli_query($connection, $query);
+}
+
+function getCommentById($connection, $id) {
+    $query = "select * from comments where id = {$id}";
+    $result = mysqli_query($connection, $query);
+    $arr = mysqli_fetch_assoc($result);
+    $comment = new Comment($arr["title"], $arr["text"], $arr["importance"], $arr["classification"], $arr["topicId"], $arr["userId"]);
+    $comment->setId($arr["id"]);
+    return $comment;
+}
+
+function getCommentByUserId($connection, $userId) {
+    $query = "select * from comments where userId = {$userId}";
+    $comments = array();
+    $result = mysqli_query($connection, $query);
+    while($arr = mysqli_fetch_assoc($result)) {
+        $comment = new Comment($arr["title"], $arr["text"], $arr["importance"], $arr["classification"], $arr["topicId"], $arr["userId"]);
+        $comment->setId($arr["id"]);
+        array_push($comments, $comment);
+    }
+    return $comments;
+}
+
+function getCommentsByTopic($connection, $topicId) {
+    $query = "select * from comments where topicId = {$topicId}";
+    $comments = array();
+    $result = mysqli_query($connection, $query);
+    while($arr = mysqli_fetch_assoc($result)) {
+        $comment = new Comment($arr["title"], $arr["text"], $arr["importance"], $arr["classification"], $arr["topicId"], $arr["userId"]);
+        $comment->setId($arr["id"]);
+        array_push($comments, $comment);
+    }
+    return $comments;
+}
