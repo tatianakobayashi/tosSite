84c85484107a455846bd732733faaf059dd49a12
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 08:34:09 -0400

    Update login-alert.php
    - set session

--- login-alert.php
+++ login-alert.php
@@ -1,12 +1,19 @@
 <?php
-    if($login) {
+    require_once("Database/connect.php");
+    require_once("Database/user-database.php");
+
+    $user = getUser($connection, $_POST["email"], $_POST["password"]);
+
+    if(isset($user)) {
+        $_SESSION["userId"]  =$user->getId();
+        $_SESSION["userName"]  =$user->getName();
 ?>
-        <p class="alert alert-success">Tópico adicionado com sucesso!</p>
+        <p class="alert alert-success">Login realizado com sucesso!</p>
 <?php
     } else {
         $msg = mysqli_error($connection);
 ?>
-        <p class="alert alert-danger">O tópico não foi adicionado: <?= $msg ?></p>
+        <p class="alert alert-danger">O login não pôde ser realizado: <?= $msg ?></p>
 <?php
     }
 ?>
