8264e54df63a5888eaeb392b6f7b17fb2a910aee
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 09:47:36 -0400

    Update navbar.php
    - user options

--- navbar.php
+++ navbar.php
@@ -15,6 +15,22 @@
             <li class="nav-item">
                 <a class="nav-link" href="topicTable.php">Tópicos Adicionados</a>
             </li>
+            
+            <?php if(isset($_SESSION["userId"])){?>
+                <li class="nav-item">
+                    <a class="nav-link" href="showUser.php"><?=$_SESSION["userName"]?></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Sair</a>
+                </li>
+            <?php }else{?>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="newUser.php">Criar Novo Usuário</a>
+                </li>
+            <?php }?>
         </ul>
     </div>
 </nav>
