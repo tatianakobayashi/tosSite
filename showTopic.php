b820da3c303f1d720c7044623913f641e782aeb1
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-11 08:47:48 -0400

    Update showTopic.php
    - code

--- showTopic.php
+++ showTopic.php
@@ -2,15 +2,26 @@
 	<title>Tradução</title>
 </head>
 <body>
-    <?php require_once("Models/Topic.php"); ?>
-	<?php require_once("navbar.php"); ?>
+    <?php require_once("Models/Topic.php");
+	require_once("navbar.php"); 
+	require_once("Database/connect.php");
+	require_once("Database/topic-database.php"); 
+	
+	if(isset($_POST["topicId"])){
+		$topic = getTopicById($connection, $_POST["topicId"]);
+	}
+	
+	?>
 
     <div>
-        <p><span><strong>Tópico original:</strong> </span></p>
-        <p><span><strong>Tradução:</strong> </span></p>
-        <form>
+        <p><span><strong>Tópico original:</strong><?=isset($topic)?$topic->getEnglish():''?></span></p>
+        <p><span><strong>Tradução:</strong><?=isset($topic)?$topic->getTranslation():''?></span></p>
+    <?php if(isset($topic)){?>
+        <form method="POST" action="editTranslation.php">
+		<input type="hidden" value="<?=$_POST["topicId"]?>">
             <button type="submit" class="btn btn-primary">Editar</button>
         </form>
+    <?php }?>
     </div>
 
 </body>
