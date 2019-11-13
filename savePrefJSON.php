a0681d8bb1dd92eaf7802756137fb28615fe351c
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-11-13 10:14:22 -0400

    Update savePrefJSON.php
    -> POST => GET

--- savePrefJSON.php
+++ savePrefJSON.php
@@ -4,8 +4,8 @@ include_once('Database/preferences-database.php');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 
-if(getPreferencesById($connection, $_POST["userId"])!== null){
-  insertPreferences($connection, $_POST["userId"], $_POST["userPreferences"])
+if(getPreferencesById($connection, $_GET["userId"])!== null){
+  insertPreferences($connection, $_GET["userId"], $_GET["userPreferences"])
 }else{
-  alterPreferences($connection, $_POST["userId"], $_POST["userPreferences"])
+  alterPreferences($connection, $_GET["userId"], $_GET["userPreferences"])
 }
